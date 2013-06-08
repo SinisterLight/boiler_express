5513214eff63f1b5d79cf1d38a806b53fb394e57
Sinister Light <me@SinisterLight.com>
2013-06-08 19:31:40 +0530

    [Shobhit] Models, views and public folder

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/app/views');
+  app.set('view engine', 'ejs');
 });
 
 app.configure('development', function(){
