14a9a9a4cafa89ea5f704b4c2add46c9538c762d
Sinister Light <me@SinisterLight.com>
2013-06-10 16:31:04 +0530

    [Shobhit] plugging in recursive require

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,4 @@
-recursive_require = require('./recursive_require');
+recursive_require = require('recursive-require');
 controllers = recursive_require(__dirname + '/../app/controllers/');
 
 module.exports = function(app) {
